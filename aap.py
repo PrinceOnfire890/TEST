06a7ff06b566fe83c3c73ffe2e9529012f32c08b
PrinceOnfire890 <princehere2010@gmail.com>
2025-05-06 21:31:45 +0530

    Update aap.py

--- aap.py
+++ aap.py
@@ -273,6 +273,5 @@ def index():
     
     return render_template_string(form_html)
 
-if _name_ == "_main_":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
